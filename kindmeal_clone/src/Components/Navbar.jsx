5f1401bc381ce9baa8d512c6f40ba6ed77d0c4fc
SuryatejaParvata <96101598+SuryatejaParvata@users.noreply.github.com>
2022-07-22 18:48:36 +0530

    kindmeal clone

---
+++ kindmeal_clone/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../Styles/Navbar.module.css"
+const Navbar = () => {
+  return (
+    <div className={styles.navcontainer}>
+testing
+    </div>
+  )
+}
+
+export default Navbar
